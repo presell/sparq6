7441f94bb3e3efde6576f975825cf54d04ef98d5
Devyn <devyn@presell.ai>
2023-03-03 21:51:53 -0500

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script
+                        async
+                        src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=HY4rVr"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default CustomDocument;
